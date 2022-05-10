896f0ee5ae66cd6a387255a492882b42da3f574e
Phil McGarty <philmcgarty@hotmail.com>
2022-05-09 22:34:52 -0400

    fixed inquirer bug and employee query

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const newDept = require('./lib/addDepartment');
 const newRole = require('./lib/addRole');
 const newEmployee = require('./lib/addEmployee');
 
-let toExit = false;
 
 // view department names and department ids
 function viewAllDepts(){
@@ -18,39 +17,42 @@ function viewAllDepts(){
             return;
         }
         console.table(rows);
-        return;    
+        mainMenu();    
     })
 };
 
 // view job title, role id, the department that role belongs to, and the salary for that role
 function viewAllRoles(){
     const sql = `
-        SELECT role.id as role_id, role.title as role_title, department.name AS department_name
+        SELECT role.id AS role_id, role.title AS role_title, department.name AS department_name
         FROM role
-        LEFT JOIN department ON role.department_id = department.id`;
+        LEFT JOIN department ON role.department_id = department.id
+        `;
     db.query(sql, (err, rows) => {
         if (err) {
             console.log(err);
             return;
         }
         console.table(rows);
-        return;    
+        mainMenu();    
     })
 };
 
 function viewAllEmployees(){
-    const sql =`
-        select employee.id, employee.first_name, employee.last_name, role.title as job_title, department.name as department_name, role.salary
-        from employee
-        inner join role on employee.role_id = role.id
-        inner join department on role.department_id = department.id`; // add manager
+    // had assistance from tutor with adding manager column
+    const sql =`        
+    SELECT e1.id, e1.first_name, e1.last_name, role.title AS job_title, department.name AS department_name, role.salary, e2.first_name AS manager_first_name, e2.last_name AS manager_last_name
+    FROM employee e1
+    LEFT JOIN role ON e1.role_id = role.id
+    LEFT JOIN department ON role.department_id = department.id
+    LEFT JOIN employee e2 ON e1.manager_id = e2.id`;
     db.query(sql, (err, rows) => {
         if (err) {
             console.log(err);
             return;
         }
         console.table(rows);
-        return;    
+        mainMenu();    
     })
 };
 
@@ -87,7 +89,7 @@ function redirect(data){
             newDept();
             break;
         case 'Add a role':
-            newRole();
+            newRole(mainMenu);
             break;
         case 'Add an employee':
             newEmployee();
@@ -97,14 +99,10 @@ function redirect(data){
             // insert query function
             break;        
         default:
-            console.log('Exit');
-            toExit = true;    
+            // following 2 lines written by tutor
+            db.end();
+            process.exit(0);    
     }
-}
-
-
-function reset(){
-    mainMenu();
 }
 
 
@@ -112,13 +110,14 @@ function mainMenu(){
     return inquirer.prompt(menuOptions)
     .then(data => {        
             return redirect(data);        
-    })
-    .then( () => {
-        if (!toExit){
-            reset();
-        }
-        return;
     })       
 };
 
-mainMenu();
+// tutor helped write this statement 
+db.connect(function(err){
+    if(err) throw err;
+    console.log("Welcome to Emp"); //change this line
+    mainMenu();
+})
+
+module.exports = mainMenu;
