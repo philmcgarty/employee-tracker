fe50e48188116f00983f8dcb8099d83d53de5bca
Phil McGarty <philmcgarty@hotmail.com>
2022-05-10 22:36:26 -0400

    tidied code, added comments

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
+// required packages
 const db = require('./db/connections');
 const inquirer = require('inquirer');
 //const mysql = require('mysql2');
 const cTable = require('console.table');
 
+// imported functions
 const newDept = require('./lib/addDepartment');
 const newRole = require('./lib/addRole');
 const newEmployee = require('./lib/addEmployee');
 const updateRole = require('./lib/updateEmployeeRole');
 
-
 // view department names and department ids
 function viewAllDepts(){
-    const sql = 'SELECT * FROM department';
+    const sql = 'SELECT id, name AS department_name FROM department';
     db.query(sql, (err, rows) => {
         if (err) {
             console.log(err);
             return;
         }
+        // display query result and return to main menu
         console.table(rows);
         mainMenu();    
     })
@@ -34,6 +36,7 @@ function viewAllRoles(){
             console.log(err);
             return;
         }
+        // display query result and return to main menu
         console.table(rows);
         mainMenu();    
     })
@@ -42,7 +45,9 @@ function viewAllRoles(){
 function viewAllEmployees(){
     // had assistance from tutor with adding manager column
     const sql =`        
-    SELECT e1.id, e1.first_name, e1.last_name, role.title AS job_title, department.name AS department_name, role.salary, e2.first_name AS manager_first_name, e2.last_name AS manager_last_name
+    SELECT e1.id, e1.first_name, e1.last_name, role.title AS job_title, 
+    department.name AS department_name, role.salary, 
+    CONCAT(e2.first_name, " ",e2.last_name) AS manager
     FROM employee e1
     LEFT JOIN role ON e1.role_id = role.id
     LEFT JOIN department ON role.department_id = department.id
@@ -52,29 +57,13 @@ function viewAllEmployees(){
             console.log(err);
             return;
         }
+        // display query result and return to main menu
         console.table(rows);
         mainMenu();    
     })
 };
 
-const menuOptions = [
-    {
-        type: 'list',
-        name: 'menuOption',
-        message: 'What would you like to do?',
-        choices: [
-            'View all departments',
-            'View all roles',
-            'View all employees',
-            'Add a department',
-            'Add a role',
-            'Add an employee',
-            'Update an employee role',
-            'Exit'
-        ]
-    }
-]
-
+// switch for menu options to direct to correct function
 function redirect(data){
     switch(data.menuOption) {
         case 'View all departments':
@@ -100,24 +89,50 @@ function redirect(data){
             break;        
         default:
             // following 2 lines provided by tutor
+            // if user chooses to exit then app ends
             db.end();
             process.exit(0);    
     }
 }
 
-
+// main menu with user options for display tables, add, etc 
 function mainMenu(){
-    return inquirer.prompt(menuOptions)
-    .then(data => {        
-            return redirect(data);        
+    return inquirer.prompt(
+        [
+            {
+                type: 'list',
+                name: 'menuOption',
+                message: 'What would you like to do?',
+                choices: [
+                    'View all departments',
+                    'View all roles',
+                    'View all employees',
+                    'Add a department',
+                    'Add a role',
+                    'Add an employee',
+                    'Update an employee role',
+                    'Exit'
+                ]
+            }
+        ]
+    )
+    .then(data => {
+        // sends input to switch statement
+        return redirect(data);        
     })       
 };
 
-// tutor helped write this statement 
+// tutor helped write this statement
+// check db, display welcome message and display main menu options 
 db.connect(function(err){
     if(err) throw err;
-    console.log("Welcome to Emp"); //change this line
+    // welcome message
+    console.log(`
+        ------------------
+            WELCOME TO
+         EMPLOYEE TRACKER
+        ------------------
+        `);
+    // calls main menu
     mainMenu();
 })
-
-module.exports = mainMenu;
